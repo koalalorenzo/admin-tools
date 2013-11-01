0e557cbdfeeba633a3935cfec3ae2e0207ce1f34
Lorenzo Setale <koalalorenzo@gmail.com>
2013-11-01 12:22:38 +0100

    Using mercurial and git and using $PATH to provide commands by default to every users (except root)

--- install.sh
+++ install.sh
@@ -6,13 +6,22 @@ sudo mkdir -p /var/domains/backups
 sudo mkdir -p /var/domains/backups/torrents 
 #I Should use one line with /backups/torrents, but is cool-er like this :P
 
-
-sudo apt-get install mercurial fail2ban apache2 libapache2-mod-wsgi libapache2-mod-proxy-html
+sudo apt-get install git mercurial fail2ban 
+sudo apt-get install apache2 libapache2-mod-wsgi libapache2-mod-proxy-html
 sudo apt-get install mysql-server phpmyadmin
 sudo apt-get install transmission-cli transmission-daemon
 
 sudo service transmission-daemon stop
 sudo echo 'manual' | sudo tee /etc/init/transmission-daemon.override
 
-sudo hg clone http://hg.setale.me/admin-tools /var/domains/tools
+sudo git clone git@github.com:koalalorenzo/admin-tools.git /var/domains/tools
 sudo bash /var/domains/tools/domain-manager-fix-permissions
+
+export PATH=$PATH:/var/domains/tools/
+cat << EOF >> /etc/profile
+
+## koalalorenzo's admin tools path:
+PATH=$PATH:/var/domains/tools/
+export PATH
+
+EOF
