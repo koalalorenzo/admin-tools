075d7902b5418332b8903567ff14908bd84502c8
Lorenzo Setale <koalalorenzo@gmail.com>
2013-11-02 20:23:48 +0100

    Created a common.sh used to setup some cool stuff for bash scripting

---
+++ common.sh
@@ -0,0 +1,53 @@
+# required variables
+BOOTUP=color
+RES_COL=70
+MOVE_TO_COL="echo -en \\033[${RES_COL}G"
+SETCOLOR_SUCCESS="echo -en \\033[1;32m"
+SETCOLOR_FAILURE="echo -en \\033[1;31m"
+SETCOLOR_WARNING="echo -en \\033[1;33m"
+SETCOLOR_NORMAL="echo -en \\033[0;39m"
+ 
+# Messages
+echo_failure() {
+    echo -n $1
+    [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+    echo -n "["
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_FAILURE
+    echo -n $"FAILED"
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
+    echo -n "]"
+    echo ""
+    return 1
+}
+
+echo_warning() {
+    echo -n $1
+    [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+    echo -n "["
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_WARNING
+    echo -n $"WARNING"
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
+    echo -n "]"
+    echo ""
+    return 1
+}
+
+echo_success() {
+    echo -n $1
+    [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+    echo -n "["
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_SUCCESS
+    echo -n $"DONE"
+    [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
+    echo -n "]"
+    echo ""
+    return 1
+}
+
+check_user(){
+    if [ $SUDO_USER ]; then user=$SUDO_USER; else user=`whoami`; fi
+    if [[ "$user" != "root" ]]; then
+        echo_failure "The installation must be launched by root or via sudo command"
+        exit 1;
+    fi
+}
